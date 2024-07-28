cd0f640f2a1a04d0b61e97e8cb77c94874ef6e44
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-07-28 02:07:44 +0200

    added more control over sorting
    Pause/resume sort
    -Visual feedback on selected algorithm
    -Todo: implement VisualArray class
    There might be bugs.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+//TODO Implement class
+export class VisualArray extends Array {}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,42 @@
 <script lang="ts">
-
+    import { Play, StopCircle } from 'lucide-svelte'
     const DEFAULT_SIZE = 50;
-    const ANIMATION_DELAY = 5;
+    //TODO: Delay slider
+    let ANIMATION_DELAY = 5; //expressed in ms
 
     let array: number[] = [];
     let size = DEFAULT_SIZE; 
     //Disable slider while sorting the array
     let sorting = false;
     let isSorted = false;
+    let stopRequested = false;
+
+    $: bubblesortSelected = selectedAlgorithm === "bubblesort";
+    $: insertionsortSelected = selectedAlgorithm === "insertionsort";
+    $: selectionsortSelected = selectedAlgorithm === "selectionsort";
+
+    let selectedAlgorithm: string = "";
 
     function generateArray() {
         isSorted = false;
-        if(array.length > 0) {
-            // reset the array
-            array = [];
-        }
-
+        array = [];
         for (let i = 0; i < size; i++) {
             array.push(Math.floor(Math.random() * 100) + 1);
         }
         console.log(array);
     }
 
-    generateArray();
-
     async function bubbleSort() {
-        generateArray();
+        
         sorting = true;
         for(let i = 0; i < array.length; i++) {
             for(let j = 0; j < array.length - i - 1; j++) {
+                /* Pause sorting */
+                if(stopRequested) {
+                    stopRequested = false;
+                    sorting = false;
+                    return;
+                }
                 await delay(ANIMATION_DELAY);
                 if(array[j] > array[j + 1]) {
                     let temp = array[j];
@@ -41,20 +49,24 @@
         isSorted = true;
     }
 
- 
- 
     function delay(ms: number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     async function insertionSort() {
-        generateArray();
         sorting = true;
         let i, key, j;
         for (i = 1; i < array.length; i++) {
             key = array[i];
             j = i - 1;
             while (j >= 0 && array[j] > key) {
+                /* Visualizer logic */
+                if(stopRequested) {
+                    stopRequested = false;
+                    sorting = false;
+                    return;
+                }
+                /* ================= */
                 await delay(ANIMATION_DELAY);
                 array[j + 1] = array[j];
                 j = j - 1;
@@ -62,19 +74,27 @@
             array[j + 1] = key;
         }
         isSorted = true;
+        sorting = false;
     }
 
-
+    console.log(array)
 
     async function selectionSort() {
-        generateArray();
         sorting = true;
         let n = array.length;
 
         for (let i = 0; i < n - 1; i++) { 
+            /* Visualizer logic */
+            if(stopRequested) {
+                stopRequested = false;
+                sorting = false;
+                return;
+            }
+            /* ================= */
             await delay(ANIMATION_DELAY); 
             let min_idx = i;
             for (let j = i + 1; j < n; j++) {
+
                 await delay(ANIMATION_DELAY); 
                 if (array[j] < array[min_idx]) {
                     min_idx = j;
@@ -84,6 +104,22 @@
             [array[min_idx], array[i]] = [array[i], array[min_idx]];
         }
         sorting = false;
+    }
+
+    function sort() {
+        switch(selectedAlgorithm) {
+            case "bubblesort":
+                bubbleSort();
+                break;
+            case "insertionsort":
+                insertionSort();
+                break;
+            case "selectionsort":
+                selectionSort();
+                break;
+            default:
+                alert("Please select an algorithm");
+        }
     }
 
     function computeHeight(value: number, max: number) {
@@ -96,19 +132,55 @@
         <h1 class="bg-blue-800 text-white text-center p-2">VisualDSA</h1>
     </nav>
     <section class="flex-[5] flex items-end gap-1 p-2 self-stretch">
-        {#each array as number, i}
-                <div id={String(number)} class="bg-green-500 flex-1 flex text-center min-w-0" style="height: {computeHeight(number, Math.max(...array))}%">
-                    <p class="flex-1 text-white" hidden={size > 69}>{number}</p>                
-                </div>
+        {#if array.length === 0}
+            <h1 class="text-black flex-1 self-center text-center text-4xl">Click on "Generate Array" to start!</h1>        
+        {/if}
+        {#each array as number}
+            <div id={String(number)} class="bg-green-500 flex-1 flex text-center min-w-0" style="height: {computeHeight(number, Math.max(...array))}%">
+                <p class="flex-1 text-white" hidden={size > 69}>{number}</p>                
+            </div>
         {/each}
+
     </section>
-    <footer class=" bg-red-800 flex flex-row">
-        <button class="flex-1 bg-blue-800 text-center hover:bg-blue-500 text-white" on:click={bubbleSort}>Bubble Sort</button>
-        <button class="flex-1 bg-blue-800 text-center hover:bg-blue-500 text-white" on:click={insertionSort}>Insertion Sort</button>
-        <button class="flex-1 bg-blue-800 text-center hover:bg-blue-500 text-white" on:click={selectionSort}>Selection Sort</button>
-        <label class="flex flex-col items-center justify-center p-3 bg-blue-950 text-white gap-2">
-            {"Size: " + size}
-            <input type="range" min="5" max="100" bind:value={size} on:input={generateArray} disabled={sorting}/>
-        </label>
+    <footer class=" bg-blue-800 flex flex-row justify-between">
+        <!-- Algorithm selection -->
+        <div class="flex">
+            <label class="flex items-center justify-center {bubblesortSelected ? "bg-blue-500" : "bg-blue-800"}  hover:bg-blue-500 text-white p-2">
+                Bubble Sort
+                <input type="radio" bind:group={selectedAlgorithm} class="appearance-none" name="alg" value="bubblesort" /><!-- on:click={bubbleSort} -->
+            </label>
+            <label class="flex items-center justify-center {insertionsortSelected ? "bg-blue-500" : "bg-blue-800"}  text-center hover:bg-blue-500 text-white p-2">
+                Insertion Sort
+                <input type="radio" bind:group={selectedAlgorithm} class="appearance-none" name="alg" value="insertionsort" /> <!--  on:click={insertionSort} -->
+            </label>
+            <label class="flex items-center justify-center {selectionsortSelected ? "bg-blue-500" : "bg-blue-800"}   text-center hover:bg-blue-500  text-white p-2">
+                Selection Sort
+                <input type="radio" bind:group={selectedAlgorithm} class="appearance-none" on:click={alert} name="alg" value="selectionsort" /> <!-- on:click={selectionSort} -->
+            </label>    
+        </div>
+
+        <!-- controls -->
+        <div class="flex text-white">
+            <button on:click={generateArray} class="flex flex-row items-center justify-center p-3 bg-blue-950 hover:bg-blue-700 text-white gap-2">
+                Generate Array
+            </button>
+            {#if sorting}
+                <button class="flex flex-row items-center justify-center p-3 bg-blue-950 hover:bg-blue-700 text-white gap-2" on:click={() => stopRequested = true}>
+                    <StopCircle /> Stop
+                </button>
+                {:else}
+                <button class="flex flex-row items-center justify-center p-3 bg-blue-950 hover:bg-blue-700 text-white gap-2" on:click={sort}>
+                    <Play /> Sort
+                </button>
+            {/if}
+            <label class="flex flex-col items-center justify-center p-3 bg-blue-950 text-white gap-2">
+                {"Size: " + size}
+                <input type="range" min="5" max="100" bind:value={size} on:input={generateArray} disabled={sorting}/>
+            </label>
+            <label class="flex flex-col items-center justify-center p-3 bg-blue-950 text-white gap-2">
+                {"Delay: " + ANIMATION_DELAY + "ms"}
+                <input type="range"  max="100" step="10" bind:value={ANIMATION_DELAY} />
+            </label>
+        </div> 
     </footer>
 </main>
