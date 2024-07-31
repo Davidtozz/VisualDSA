44e5a9bd36b02232e0d46eb1d4f40501c3f81f52
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-07-31 16:54:18 +0200

    fix: goto can't be called on the server

--- src/routes/visualizer/+page.svelte
+++ src/routes/visualizer/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-    import { dev } from "$app/environment";
-    import { goto } from "$app/navigation";
     import ArrayVisualizer from "@/components/ArrayVisualizer.svelte";
-
-    if(!dev) goto('/')
-
 </script>
 
-{#if dev} 
-    <ArrayVisualizer />
-{/if}
+<ArrayVisualizer />
