f12e8b1fe1670f31759d2c6222d2a53b2f7c41b6
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-07-31 16:37:53 +0200

    svelte-canvas working quicksort demo

---
+++ src/routes/visualizer/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { dev } from "$app/environment";
+    import { goto } from "$app/navigation";
+    import ArrayVisualizer from "@/components/ArrayVisualizer.svelte";
+
+    if(!dev) goto('/')
+
+</script>
+
+{#if dev} 
+    <ArrayVisualizer />
+{/if}
