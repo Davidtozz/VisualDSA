366751127e62885962af92de7c9ce95022c92c66
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-08-03 20:55:14 +0200

    add: svelte-highlight, shadcn resizable

---
+++ src/lib/components/FunctionPreview.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { writable } from 'svelte/store';
+    import hljs from 'highlight.js/lib/core';
+    import 'highlight.js/styles/github-dark.min.css'
+    import {bubbleSort} from '$lib/algorithms/sort';
+    import { visualizerFlags } from '@/stores';
+    import Highlight, {LineNumbers} from 'svelte-highlight';
+    import javascript from "svelte-highlight/languages/javascript"
+    import * as Resizable from '@shadcn/resizable'
+    import { algorithm } from '@/stores';
+    import { sortingFunctions } from '$lib/algorithms/sort';
+
+
+    export let fontSize = writable<number>(12);
+    $: func = sortingFunctions.find(fn => fn.name === $algorithm)?.fn.toString() ?? '// Click on a function to view its code!';
+    
+    let currentStep = writable();
+    const className: string = '';
+    export { className as class};
+</script>
+
+
+    
+<Highlight language={javascript} code={func} let:highlighted>
+    <LineNumbers {highlighted} wrapLines={true}
+    highlightedLines={[$currentStep]}
+    --highlighted-background-color="#2d2d2d"
+    style="font-size: {$fontSize}px; flex-grow:1;"
+    />
+</Highlight>    
+      
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,23 @@
 
 </script>
 
-<div class="h-dvh flex flex-row bg-primary">
-    <Sidebar class="hidden lg:flex border-gray-800 border-r-[1px]"/>
-    <main class="h-dvh flex flex-col overflow-auto bg-primary flex-[10]">
-        <Header />
-        <Visualizer class="flex-[5] flex items-end self-stretch overflow-hidden shadow-md" />
-        <Footer class="bg-gray-800 flex flex-row" />
-    </main>
+<div class="h-dvh bg-primary">
+    <Resizable.PaneGroup direction="horizontal">
+        <Resizable.Pane {...paneConfig}>
+            <Sidebar class="flex-col hidden lg:flex border-gray-800 border-r-[1px]"/>
+        </Resizable.Pane>
+        <Resizable.Handle withHandle/>
+        <Resizable.Pane>
+            <main class="h-dvh flex flex-col overflow-auto bg-primary flex-[10]">
+                <Header />
+                <Visualizer class="flex-[5] flex items-end self-stretch overflow-hidden shadow-md" />
+                <Controls class="bg-gray-800 flex flex-row" />
+            </main>
+        </Resizable.Pane>
+        <Resizable.Handle/>
+        <Resizable.Pane>
+            <FunctionPreview class="max-w-32 " />
+        </Resizable.Pane>
+    </Resizable.PaneGroup>
+
 </div>
